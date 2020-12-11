df24a798f4857ed05bd08c3c441431f070cd2641
kendraarsena <kendraarsena@ymail.com>
2020-12-11 05:12:21 -0800

    Update ViewController.swift

--- projectlab/ViewController.swift
+++ projectlab/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("tes")
         // Do any additional setup after loading the view.
     }
 
