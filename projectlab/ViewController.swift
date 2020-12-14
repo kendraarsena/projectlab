64fb5b44fc2a298eb2f9c752f4036bf39f212cc3
nataliafellyana <49163735+nataliafellyana@users.noreply.github.com>
2020-12-14 08:25:17 -0800

    Update ViewController.swift

--- projectlab/ViewController.swift
+++ projectlab/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+var welcome:String = ""
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var txtUsername: UITextField!
@@ -18,15 +20,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnLogin(_ sender: Any) {
-        performSegue(withIdentifier: "home", sender: self)
+        let usernameStored = UserDefaults.standard.string(forKey: "username")
+        let passwordStored = UserDefaults.standard.string(forKey: "password")
+        
+        if((txtUsername.text!.isEmpty) || (txtPassword.text!.isEmpty)){
+            showAlert(title: "Error", message: "All fields must be filled!")
+        }
+        else if((txtUsername.text! != usernameStored) || (txtPassword.text! != passwordStored)){
+            showAlert(title: "Error", message: "Username / Password is incorrect")
+        }
+        else{
+            welcome = "Welcome, \(txtUsername.text as! String)"
+            performSegue(withIdentifier: "home", sender: self)
+        }
     }
     
     @IBAction func btnRegister(_ sender: Any) {
         performSegue(withIdentifier: "register", sender: self)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 }
 
