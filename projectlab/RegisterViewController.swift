0d39b2cdda07e53a62093be8048613c848852e98
nataliafellyana <49163735+nataliafellyana@users.noreply.github.com>
2020-12-14 08:25:21 -0800

    Update RegisterViewController.swift

--- projectlab/RegisterViewController.swift
+++ projectlab/RegisterViewController.swift
@@ -37,6 +37,9 @@ class RegisterViewController: UIViewController {
         } else if (phone.count != 12) {
             showAlert(title: "Error", message: "Phone number must be 12 digits")
         } else {
+            UserDefaults.standard.set(username, forKey: "username")
+            UserDefaults.standard.set(password, forKey: "password")
+            UserDefaults.standard.synchronize()
             performSegue(withIdentifier: "login", sender: self)
         }
     }
