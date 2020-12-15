3def0c58dbb583b1c239396359266ae8a0b47c16
nataliafellyana <49163735+nataliafellyana@users.noreply.github.com>
2020-12-14 23:50:19 -0800

    Update CreateViewController.swift

--- projectlab/CreateViewController.swift
+++ projectlab/CreateViewController.swift
@@ -109,8 +109,9 @@ class CreateViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     @objc func done(){
         let format = DateFormatter()
-        format.dateStyle = .medium
-        format.timeStyle = .none
+        format.dateFormat = "dd-MM-YYYY"
+//        format.dateStyle = .medium
+//        format.timeStyle = .none
         
         txtPublish.text = format.string(from: datePicker.date)
         self.view.endEditing(true)
